1c413da6c9abbd3125a465d705db8f597468f456
Harish Naidu BN <harishbnpvg@gmail.com>
2026-09-21 19:16:33 +0530

    Home work

---
+++ variablesHW.java
@@ -0,0 +1,20 @@
+class variablesHW
+{
+    public static void main(String args[])
+    {
+        int gta =6;
+        double pi=3.14159;
+        char gender ='M';
+        String name ="Bro code";
+        boolean isAdmin =true;
+
+        System.out.println(gta);
+        System.out.println(pi);
+        System.out.println(name);
+        if (isAdmin)
+        System.out.println("Yes");
+        else
+        System.out.println("NO");
+
+    }
+}
